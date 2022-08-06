fc146fa2681e930248ba8b532cd909f256e1ced5
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-06 13:44:56 -0400

    Improves build script and works on aboutme

--- make.cmd
+++ make.cmd
@@ -1,6 +1,11 @@
-cd %jobpath%
-pushd resume_generator
-wsl make
-popd
-cd output
-start.
+If not "%Jobpath%"=="%Jobpath:resume=%" (
+    @REM cd %jobpath% 
+    @REM pushd resume_generator
+    @REM git add . && git tag "Build #%resume_build%" && git commit -m "%Build commit for #%resume_build%"
+    @REM wsl make
+    @REM popd
+    @REM cd output
+    @REM start.
+) else (
+    echo Please Run this script from Project root direcory.
+)
