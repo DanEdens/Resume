7e0eb499caac7d39cff4b2f6152941790cb925f8
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-20 22:26:53 -0400

    fixes rushed install remote commit

--- CodePenPDF/Program.cs
+++ CodePenPDF/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Xml.Linq;
+
+namespace Selenium_Automation
+{
+    class Program
+    {
+        private const string ChromeDriverDirectory = "C:\\Users\\Dan\\bin\\ChromeDriver";
+        private static object? jobGeo;
+
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(ChromeDriverDirectory);
+
+            // This will open up the URL
+            driver.Url = "https://codepen.io/danedens/full/ZExrjqV";
+            System.Threading.Thread.Sleep(10000);
+            IWebElement element = driver.FindElement(By.Id("body > div:nth-child(3) > div > div.section.middle.clearfix > div > h2 > i"));
+            
+            element.Click();
+        }
+    }
+}
